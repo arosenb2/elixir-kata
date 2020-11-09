210f52ff2f9a6aebbec54c3dcf85dcae0c77bc70
Aaron Rosenbaum <aaron.rosenbaum@gmail.com>
2020-11-09 15:14:11 -0500

    feat: add dubstep kata

---
+++ test/song_decoder_test.exs
@@ -0,0 +1,21 @@
+defmodule SongDecoderTest do
+  use ExUnit.Case
+
+  import SongDecoder, only: [decode_song: 1]
+
+  @moduletag :capture_log
+
+  doctest SongDecoder
+
+  test "WUB should be replaced by 1 space" do
+    assert SongDecoder.decode_song("AWUBBWUBC") == "A B C"
+  end
+
+  test "Multiples WUB should be replaced by only 1 space" do
+    assert SongDecoder.decode_song("AWUBWUBWUBBWUBWUBWUBC") == "A B C"
+  end
+
+  test "Heading or trailing spaces should be removed" do
+    assert SongDecoder.decode_song("WUBAWUBBWUBCWUB") == "A B C"
+  end
+end
