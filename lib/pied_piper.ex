292677b3631de75c739882078a788796a3fac804
Aaron Rosenbaum <aaron.rosenbaum@gmail.com>
2020-11-24 14:30:28 -0500

    fix: adjust formatting on pied piper to allow compile

--- lib/pied_piper.ex
+++ lib/pied_piper.ex
@@ -5,34 +5,29 @@ defmodule PiedPiper do
   ## Story
   The Pied Piper has been enlisted to play his
   magical tune and coax all the rats out of town.
-
   But some of the rats are deaf and are going the
   wrong way!
-
   ## Kata Task
   How many deaf rats are there?
-
   ## Legend
   P = The Pied Piper
   O~ = Rat going left
   ~O = Rat going right
-
   ## Example
   iex> count_deaf_rats("~O~O~O~O P")
   0
   iex> count_deaf_rats("P O~ O~ ~O O~")
   1
-
   iex> count_deaf_rats("~O~O~O~OP~O~OO~")
   2
   """
   def count_deaf_rats(s) do
     s
     |> String.split("P")
-    |> fn [left, right] do
-      count(left) + count(String.reverse(right))
-    end.()
+    |> parse()
   end
+
+  defp parse([left, right]), do: count(left) + count(String.reverse(right))
 
   defp count(str) do
     str
